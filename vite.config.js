7368487c029615f778d14049f570c2f0e52f7ea0
dozelix <wsamuelixyw@gmail.com>
2026-06-02 23:00:22 -0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/act1Unit3Front/', // <-- AGREGA ESTA LÍNEA (con los slashes al inicio y al final)
 })
